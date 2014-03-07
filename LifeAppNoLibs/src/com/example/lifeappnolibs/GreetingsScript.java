29abbfb0787db768976d63780a6d5d1b633ff26d
sellnat77 <sell_nat@yahoo.com>
2014-03-06 20:09:27 -0800

    test

--- LifeAppNoLibs/src/com/example/lifeappnolibs/GreetingsScript.java
+++ LifeAppNoLibs/src/com/example/lifeappnolibs/GreetingsScript.java
@@ -2,14 +2,21 @@ package com.example.lifeappnolibs;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.DialogInterface.OnClickListener;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
-public class GreetingsScript extends Activity {
+public abstract class GreetingsScript extends Activity implements OnClickListener {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_greetings_script);
+		
+		Button writtenSampButton = (Button) findViewById(R.id.written_samples);
+		writtenSampButton.setOnClickListener((android.view.View.OnClickListener) this);
 	}
 
 	@Override
@@ -17,6 +24,19 @@ public class GreetingsScript extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.greetings_script, menu);
 		return true;
+	}
+
+	public void onClick(View theView)
+	{
+		int id = theView.getId();
+		
+		if(id == R.id.written_samples)
+		{
+			Intent writtenIntent = new Intent(this,WrittenSamples.class);
+			startActivity(writtenIntent);
+		}
+		
+		
 	}
 
 }
